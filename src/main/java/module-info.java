89c15531d6392b745e396ac35ba075c295e77622
13100d <3lithium6.9@gmail.com>
2023-11-18 15:01:26 +0530

    HOLY SHIT IT WORKS AHAHSSADHKJADSKJHDSA

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.example.controller {
     requires javafx.controls;
     requires javafx.fxml;
+    opens com.example.controller to javafx.fxml;
+    exports com.example.controller;
     opens com.example to javafx.fxml;
     exports com.example;
 }
