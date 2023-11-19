7da184e3662caaa4282a7601893ffee4e9e16abc
riyasach189 <riyasach189@gmail.com>
2023-11-19 17:13:16 +0530

    saveCherries code

--- src/main/java/com/example/controller/Cherry.java
+++ src/main/java/com/example/controller/Cherry.java
@@ -1,4 +1,19 @@
 package com.example.controller;
 
 public class Cherry {
+    private static int numCherries;
+
+    public static int getNumCherries() {
+        return numCherries;
+    }
+
+    public void incrCherries()
+    {
+
+    }
+
+    public void reviveSubtractCherries()
+    {
+
+    }
 }
--- src/main/java/com/example/controller/GameOverScreen.java
+++ src/main/java/com/example/controller/GameOverScreen.java
@@ -5,6 +5,11 @@ import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Objects;
+
 public class GameOverScreen extends ControllerBase {
 
     @FXML
@@ -29,26 +34,26 @@ public class GameOverScreen extends ControllerBase {
         System.out.println("Switching back to game");
         stage.getScene().setRoot(loadFXML("/GameScreen.fxml"));
     }
-    private void switchToMainScreen() { // back to main menu
+
+    private void switchToMainScreen() { // back to the main menu
         System.out.println("Switching to mainScreen");
         stage.getScene().setRoot(loadFXML("/MainScreen.fxml"));
     }
 
-//    private void toggleVolume() {
-//        isMuted = !isMuted;
-//        updateVolumeButtonImage();
-//        // Add logic to control audio volume based on the 'isMuted' state
-//        // For simplicity, we'll just print a message here
-//        System.out.println("Volume is " + (isMuted ? "muted" : "unmuted"));
-//    }
+    private void saveCherries() {
+        try (PrintWriter writer = new PrintWriter(new FileWriter("cherries.txt"))) {
+            int cherries = Cherry.getNumCherries();
+            writer.println(cherries);
+            System.out.println("Cherries saved to file: " + cherries);
+        }
 
-//    private void updateVolumeButtonImage() {
-//        String imageName = isMuted ? "/soundmutedicon.png" : "/soundyesicon.png";
-//        setButtonImage(volumeButton, imageName);
-//    }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     private Image getImage(String imageName) {
-        return new Image(getClass().getResourceAsStream(imageName));
+        return new Image(Objects.requireNonNull(getClass().getResourceAsStream(imageName)));
     }
 
     private void setButtonImage(Button button, String imageName) {
