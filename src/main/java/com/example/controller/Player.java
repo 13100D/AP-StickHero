81b3e42c8c632e00133b2947bb3f71e906934c73
riyasach189 <riyasach189@gmail.com>
2023-11-27 15:02:10 +0530

    singleton

--- src/main/java/com/example/controller/Player.java
+++ src/main/java/com/example/controller/Player.java
@@ -3,6 +3,21 @@ package com.example.controller;
 public class Player {
 
     private int currentScore;
+    private static Player player = null;
+
+    //singleton - Design Practice
+    public static Player getInstance()
+    {
+        if (player == null) {
+            player = new Player();
+        }
+
+        return player;
+    }
+
+    private Player() {
+        this.currentScore = 0;
+    }
 
     public void extendStick()
     {
