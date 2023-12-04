a4a33bff9343a9bfaf278d45563b4b2c3e64ae7a
Riya Sachdeva <riyasach189@gmail.com>
2023-12-04 20:42:36 +0530

    gyhiui

--- src/main/java/com/example/controller/Cherry.java
+++ src/main/java/com/example/controller/Cherry.java
@@ -1,6 +1,8 @@
 package com.example.controller;
 
+import javafx.animation.FadeTransition;
 import javafx.scene.image.ImageView;
+import javafx.util.Duration;
 
 import java.util.*;
 
@@ -9,10 +11,11 @@ public class Cherry {
     private static final int spawnRate = 43;
     private static Map<Integer, Cherry> instances = new HashMap<>();
     private static ImageView cherrySprite;
-    private int distance;
+    private final int distance;
 
     private Cherry(int distance)
     {
+        this.distance = distance;
     }
 
     public static Cherry getInstance(Integer distance) {
@@ -36,5 +39,22 @@ public class Cherry {
     public static void reviveSubtractCherries()
     {
         numCherries -= 20;
+    }
+
+    public void fadeInAnimation() {
+        FadeTransition fadeIn = new FadeTransition(Duration.millis(1000), cherrySprite);
+        fadeIn.setFromValue(0.0);
+        fadeIn.setToValue(1.0);
+        fadeIn.play();
+    }
+
+    public void fadeOutAnimation() {
+        FadeTransition fadeOut = new FadeTransition(Duration.millis(1000), cherrySprite);
+        fadeOut.setFromValue(1.0);
+        fadeOut.setToValue(0.0);
+        fadeOut.setOnFinished(event -> {
+            // Additional logic to handle animation completion, if needed
+        });
+        fadeOut.play();
     }
 }
