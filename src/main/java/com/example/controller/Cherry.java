540c11822560c2fff12503f0aac9151e68da7a1a
13100D <3lithium6.9@gmail.com>
2023-12-04 20:44:15 +0530

    add in visible javafx properties

--- src/main/java/com/example/controller/Cherry.java
+++ src/main/java/com/example/controller/Cherry.java
@@ -1,6 +1,8 @@
 package com.example.controller;
 
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 
 import java.util.*;
 
@@ -8,11 +10,20 @@ public class Cherry {
     private static int numCherries;
     private static final int spawnRate = 43;
     private static Map<Integer, Cherry> instances = new HashMap<>();
-    private static ImageView cherrySprite;
+    private static ImageView cherrySprite = new ImageView(new Image("com/example/assets/coin.png"));
     private int distance;
+
+    public static ImageView getCherrySprite() {
+        return cherrySprite;
+    }
 
     private Cherry(int distance)
     {
+        this.distance = distance;
+        cherrySprite.setFitHeight(35);
+        cherrySprite.setFitWidth(35);
+        cherrySprite.setLayoutY(500);
+        cherrySprite.setLayoutX((distance)+200); //random upper bound as distance
     }
 
     public static Cherry getInstance(Integer distance) {
